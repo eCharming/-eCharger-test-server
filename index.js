9d809c4f6e7a388887a8c6f8fba3588aebabd257
D-Sketon <2055272094@qq.com>
2023-03-04 17:34:56 +0800

    feat: 添加charger测试

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express')
-const app=express()
-app.get('/api/mock',(err,res)=>{
+const app = express()
+app.get('/api/chargers', (err, res) => {
     console.log("get")
     res.send({
-        code:200,
+        code: 200,
         message: null,
-        data: {name: "foo", age:18}
+        data: {
+            chargers: [{
+                location: "北京",
+                detailLocation: "北京市海淀区西城15道",
+                price: "0.1",
+                availableTime: "00:00-10:00",
+                distance: "10.5"
+            }, {
+                location: "北京",
+                detailLocation: "北京市海淀区西城15道",
+                price: "0.1",
+                availableTime: "00:00-10:00",
+                distance: "10.5"
+            }]
+        }
     })
 })
-app.listen(8080,()=>{
+app.get('/api/mock', (err, res) => {
+    console.log("get")
+    res.send({
+        code: 200,
+        message: null,
+        data: {name:"foo",age:18}
+    })
+})
+app.listen(8080, () => {
     console.log('Success');
 })
